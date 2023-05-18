15e634697c5b04d467cf5ac9b98e3c740f47c490
Maximiliano Verón <maxi.ush.ind@gmail.com>
2023-05-18 12:39:07 -0300

    Carga de 4 usuarios a la tabla

--- Tienda.sql
+++ Tienda.sql
@@ -1,9 +1,12 @@
+-- Creacion base de datos
 
 CREATE DATABASE IF NOT EXISTS tienda;
 
 USE tienda;
 
-CREATE TABLE Usuarios (
+-- Creacion tabla 
+
+CREATE TABLE IF NOT EXISTS usuarios (
 
   id bigint(3),
   nombre varchar(20),
@@ -12,3 +15,16 @@ CREATE TABLE Usuarios (
   correo varchar(40),
   clave varchar(20)  
 );
+
+-- Create
+
+INSERT INTO usuarios values (1, "raul", "fernandez", "123456789", "raul@gmail.com", "asd123");
+INSERT INTO usuarios values (2, "emanuel", "martinez", "123456788", "ema@gmail.com", "emamartinez");
+INSERT INTO usuarios values (3, "azul", "lopez", "123456787", "azul@hotmail.com", "lopez123");
+INSERT INTO usuarios values (4, "abril", "diaz", "123456786", "abril@yahoo.com", "654321");
+
+-- Read
+
+-- Update
+
+-- Delete
