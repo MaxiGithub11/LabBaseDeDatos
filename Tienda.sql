ab5d5a3e2c038e507a16eaa36de4840cca960624
Maximiliano Verón <maxi.ush.ind@gmail.com>
2023-05-18 12:24:33 -0300

    Generacion tabla Usuarios con su estructura de datos

--- Tienda.sql
+++ Tienda.sql
@@ -2,3 +2,13 @@
 CREATE DATABASE IF NOT EXISTS tienda;
 
 USE tienda;
+
+CREATE TABLE Usuarios (
+
+  id bigint(3),
+  nombre varchar(20),
+  apellido varchar(20),
+  celular varchar(20),
+  correo varchar(40),
+  clave varchar(20)  
+);
